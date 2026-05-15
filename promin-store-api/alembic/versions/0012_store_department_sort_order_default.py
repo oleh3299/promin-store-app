65cda35a4ddc731364c93715a7aa3b35a8dffda5
oleh3299 <abcsvitlana326@gmail.com>
2026-05-15 08:43:00 +0300

    Fix store task migration revision id

--- promin-store-api/alembic/versions/0012_store_department_sort_order_default.py
+++ promin-store-api/alembic/versions/0012_store_department_sort_order_default.py
@@ -1,12 +1,12 @@
 """set store department sort order default
 
-Revision ID: 0012_store_department_sort_order_default
+Revision ID: 0012_store_dept_order
 Revises: 0011_store_tasks_foundation
 Create Date: 2026-05-14
 """
 from alembic import op
 
-revision = "0012_store_department_sort_order_default"
+revision = "0012_store_dept_order"
 down_revision = "0011_store_tasks_foundation"
 branch_labels = None
 depends_on = None
